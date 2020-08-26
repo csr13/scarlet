21bdb4685946ba7550b5769a0c839aec12381007
csr13 <carsaroca.13@gmail.com>
2020-08-26 03:40:00 -0500

    init commit

---
+++ game/characters.py
@@ -0,0 +1,65 @@
+"""~+~+[csr]+~+~"""
+
+
+import os
+import random
+
+import pygame
+from pygame.locals import *
+
+
+IMAGES = os.path.join("assets", "images")
+
+
+class Skarlet(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.img = pygame.image.load(os.path.join(IMAGES, "skarlet.png"))
+        self.rect = self.img.get_rect()
+        self.alive = True
+        self.life_points = 300
+        self.speed = 20
+
+    def state(self, key):
+        if key[K_UP]:
+             pass 
+        if key[K_DOWN]:
+            pass 
+        if key[K_RIGHT]:
+            pass
+        if key[K_LEFT]:
+            pass
+        return true 
+
+    def update(self):
+        pass 
+
+
+class Star(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+    
+    def update(self):
+        return 
+
+
+class Comet(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+    def update(self):
+        return 
+
+
+class Enemy(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+    def update(self):
+        return
+
+
---
+++ game/play.py
@@ -0,0 +1,58 @@
+"""~+~+[csr]+~+~"""
+
+
+import pygame
+from pygame.locals import *
+
+from characters import (
+    Comet,
+    Enemy,
+    Skarlet,
+    Star
+)
+
+
+class Game(object):
+
+    DIMENSIONS = (696, 696)
+
+    def __init__(self):
+        pygame.init()
+        pygame.key.set_repeat(10, 100)
+        pygame.display.set_caption("<Cyber * Skarlet>")
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode(self.DIMENSIONS, RESIZABLE)
+        self.skarlet = Skarlet()
+
+    def main(self) -> None:
+        running = True
+        while running:
+            for event in pygame.event.get():
+                pygame.event.pump()
+                key = pygame.key.get_pressed()    
+                if key[K_ESCAPE]:
+                    running = False
+            self.screen.fill((14, 1, 40,))
+            self.screen.blit(self.skarlet.img, self.skarlet.rect)
+            pygame.display.update()
+            self.clock.tick(696)
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    Game().main()
+
+    
+    
+
+
+    
+
+    
+
+
+
+    
+
+
+
