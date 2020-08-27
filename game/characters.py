1cb8407b7b3b140af892ea1b47ce739b100d8cbf
csr13 <carsaroca.13@gmail.com>
2020-08-27 03:05:16 -0500

    Skarlet work

--- game/characters.py
+++ game/characters.py
@@ -11,7 +11,14 @@ IMAGES = os.path.join("assets", "images")
 
 
 class Skarlet(pygame.sprite.Sprite):
+    """
+    ~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+
+    <{+}> Mess with the best, die like the rest. <{+}>
+    ~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+
+    """
     
+    DIMENSIONS = (696, 696,)
+
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.img = pygame.image.load(os.path.join(IMAGES, "skarlet.png"))
@@ -19,20 +26,34 @@ class Skarlet(pygame.sprite.Sprite):
         self.alive = True
         self.life_points = 300
         self.speed = 20
-
-    def state(self, key):
-        if key[K_UP]:
-             pass 
-        if key[K_DOWN]:
-            pass 
-        if key[K_RIGHT]:
-            pass
-        if key[K_LEFT]:
-            pass
-        return true 
-
-    def update(self):
-        pass 
+        self.keys = {
+            K_RIGHT: (self.speed, 0), 
+            K_LEFT: (-self.speed, 0), 
+            K_DOWN: (0, self.speed),
+            K_UP: (0, -self.speed), 
+        }
+        self.rect[0], self.rect[1] = (696 // 2, 660)
+    
+    def get_position(self) -> tuple:
+        return self.rect[0], self.rect[1]
+    
+    def check_screen_boundaries(self) -> None:
+        x, y = self.get_position()
+        if x <= 0:
+            self.rect[0] = 1
+        if x >= 660:
+            self.rect[0] = 660
+        if y >= 660:
+            self.rect[1] = 660
+        if y <= 0:
+            self.rect[1] = 1
+    
+    def state(self, key: int) -> bool:
+        for move, vector in self.keys.items():
+            if key[move]:
+                self.rect.move_ip(vector)
+        self.check_screen_boundaries()
+        return True
 
 
 class Star(pygame.sprite.Sprite):
