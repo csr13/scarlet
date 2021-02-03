e537d5b24ed8b931a407fac67c0027b462211731
csr13 <carsaroca.13@gmail.com>
2021-02-03 01:31:38 -0600

    [+] Light changes on typing

--- game/play.py
+++ game/play.py
@@ -4,10 +4,8 @@ import pygame
 from pygame.locals import *
 
 from characters.comet import Comet
-from characters.enemy import Enemy 
-from characters.laser import Laser
 from characters.skarlet import Skarlet
-from characters.star import Star 
+from characters.star import Star
 
 
 class Game(object):
@@ -25,40 +23,43 @@ class Game(object):
         self.comets = pygame.sprite.Group()
         self.enemies = pygame.sprite.Group()
 
-    def _update(self) -> None:
-        for comet in self.comets:comet.update()
-        for star in self.stars: star.update()
+    def _update(self):
+        for comet in self.comets:
+            comet.update()
+        for star in self.stars:
+            star.update()
 
-    def _draw(self) -> None:
-        for comet in self.comets: comet.draw(self.screen)
-        for star in self.stars: star.draw(self.screen)
-    
-    def _blit(self, color: tuple=(0, 0, 69)) -> None:
+    def _draw(self):
+        for comet in self.comets:
+            comet.draw(self.screen)
+        for star in self.stars:
+            star.draw(self.screen)
+
+    def _blit(self, color=(0, 0, 69)):
         self.screen.fill(color)
         self.screen.blit(self.skarlet.image, self.skarlet.rect)
-    
+
     def _replicate(self):
         Comet.factory(self.comets)
         Star.factory(self.stars)
 
-    def main(self) -> None:
+    def main(self):
         running = True
         while running:
             for event in pygame.event.get():
                 pygame.event.pump()
-                key = pygame.key.get_pressed()    
+                key = pygame.key.get_pressed()
                 if key[K_ESCAPE]:
                     running = False
                 self.skarlet.state(key)
             self._replicate()
-            self._blit() 
+            self._blit()
             self._update()
             self._draw()
             pygame.display.update()
             self.clock.tick(1500)
         pygame.quit()
-    
+
 
 if __name__ == "__main__":
     Game().main()
-
